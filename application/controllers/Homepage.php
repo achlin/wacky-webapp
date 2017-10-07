dec0b1be55e5a1a4a6901ef14f885b6fc603a553
Kenneth Culala <kaculala@gmail.com>
2017-10-07 15:39:06 -0700

    switch leading tabs to spaces in Homepage controller

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -5,15 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Homepage extends Application
 {
 
-	/**
-	 * Index Page for this controller.
-	 */
-	public function index()
-	{
-		$this->data['pagebody'] = 'homepage';
-		$this->data['fleetsize'] = $this->fleet->size();
-		//$this->render();
-		$this->render();
-	}
+    /**
+     * Index Page for this controller.
+     */
+    public function index()
+    {
+        $this->data['pagebody'] = 'homepage';
+        $this->data['fleetsize'] = $this->fleet->size();
+        $this->render();
+    }
 
 }
