a85ac867f3407b3a1b74ae691b4fe1fad33b1492
Kenneth Culala <kaculala@gmail.com>
2017-10-07 22:57:02 -0700

    finish up homepage. style template and homepage. Closes #5

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,4 +1,42 @@
-<div>
-    <p>We'll get you there in a flash!</p>
-    <p>There are {fleetsize} planes in our fleet.</p>
-</div>
+<div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+            <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                    <h4 class="card-title">Base</h4>
+                    <p class="card-text">We operate from {baselocation} ({basecode})<br></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+            <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                    <h4 class="card-title">Serviced Airports</h4>
+                    <p class="card-text">
+                        {airports}
+                        {location} ({code}) <br>
+                        {/airports}
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+            <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                    <h4 class="card-title">Flights</h4>
+                    <p class="card-text">{flightsize} flights scheduled daily<br></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+            <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                    <h4 class="card-title">Fleet</h4>
+                    <p class="card-text">Growing fleet of {fleetsize} planes</p>
+                </div>
+            </div>
+        </div>
+    </div>
