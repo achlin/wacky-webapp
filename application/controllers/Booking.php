dd957dfc5d020c66c3c8e17561a1ec6acc21d031
Andy Lin <andylin1218@gmail.com>
2017-11-13 20:27:52 -0800

    Changed booking search layout

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -11,16 +11,19 @@ class Booking extends Application
     private $visted = array();
     private $allAirports;
     const MAX_PATH_LENGTH = 3;
+    private $timezone;
 
     function __construct()
     {
         parent::__construct();
+        $this->timezone =  new DateTimeZone('America/Vancouver');
     }
 
     /**
      * Search for paths to and from the user selected airports.
      */
     public function search() {
+        $form = $this->input->post();
         $this->allAirports = $this->airports->airportsWeService();
         $this->initAdj();
         $this->data['availableFlights'] = array();
@@ -33,41 +36,75 @@ class Booking extends Application
         $result = $this->findPaths();
 
         foreach ($result as $key => $value) {
-            $processedPath = $this->processPaths($key, $value);
+            $processedPath = $this->processPaths($form, $key, $value);
             if (!empty($processedPath['flights'])) {
                 array_push($this->data['availableFlights'], $processedPath);
             }
         }
+
+        $this->data['departsFrom'] = $form['startAirport'];
+        $this->data['departureCity'] = $this->allAirports[$form['startAirport']]->community;
+        $this->data['arrivesAt'] = $form['endAirport'];
+        $this->data['arrivalCity'] = $this->allAirports[$form['endAirport']]->community;
+
         $this->showit();
     }
 
     /**
      * Process found paths into a format that's displayable.
      */
-    private function processPaths($key, $value) {
-        $form = $this->input->post();
+    private function processPaths($form, $key, $value) {
         $flights = array();
+        $airports = $this->airports->airportsWeService();
+        $arrow = '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>';
         $tempflights = $value[$form['startAirport']][$form['endAirport']];
+
         foreach($tempflights as $tempflight) {
-            $segmentIds = '';
+            $cityPath = '';
             $flightPathId = '';
             $flight = array();
+            $totalDepartureDate;
+            $lastSegmentArrivalTime = '00:00';
+            $daysCount = 0;
             foreach($tempflight as $segment) {
                 array_push($flight, (array) $segment);
-                if ($segmentIds) {
-                    $segmentIds .= " -> ";
+                if ($cityPath) {
+                    $cityPath .= " " . $arrow . " " . $this->allAirports[$segment->arrivesAt]->community;
+                } else {
+                    $departureCity = $this->allAirports[$segment->departsFrom]->community;
+                    $arrivalCity = $this->allAirports[$segment->arrivesAt]->community;
+                    $cityPath .= $departureCity . " " . $arrow . " " . $arrivalCity;
+                    $totalDepartureDate = DateTime::createFromFormat('H:i', $segment->departureTime, $this->timezone);
                 }
-                $segmentIds .= $segment->id;
+
+                $segmentDepartDate = DateTime::createFromFormat('H:i', $segment->departureTime, $this->timezone);
+                $lastSegmentArrivalDate = DateTime::createFromFormat('H:i', $lastSegmentArrivalTime, $this->timezone);
+
+                if ($lastSegmentArrivalDate >= $segmentDepartDate) {
+                    $daysCount++;
+                }
+                $lastSegmentArrivalTime = $segment->arrivalTime;
                 $flightPathId .= $segment->id;
             }
+
+            $totalArrivalDate = DateTime::createFromFormat('H:i', $lastSegmentArrivalTime, $this->timezone);
+
+            $totalDepartureDateF = $this->dateFormatting($totalDepartureDate, $daysCount);
+            $totalArrivalDateF = $this->dateFormatting($totalArrivalDate, ++$daysCount);
+
             array_push($flights, array('flight' => $flight,
-                            'segmentIds' => $segmentIds,
+                            'totalDepartureDate' => $totalDepartureDateF,
+                            'totalArrivalTime' => $totalArrivalDateF,
+                            'cityPath' => $cityPath,
                             'flightPathId' => $flightPathId));
         }
         return array('flights' => $flights,
-                    'departsFrom' => $form['startAirport'],
-                    'arrivesAt' => $form['endAirport'],
                     'NoOfStops' => $key-1);
+    }
+
+    private function dateFormatting($date, $daysToAdd) {
+        $date->add(new DateInterval('P' . $daysToAdd . 'D'));
+        return $date->format('Y-m-d H:i T');
     }
 
     /**
--- application/views/booking.php
+++ application/views/booking.php
@@ -1,17 +1,23 @@
+<h4 class="text-white">Flights from {departureCity}({departsFrom}) to {arrivalCity}({arrivesAt})</h4>
 {availableFlights}
 <div class="row">
     <table class="table table-striped table-dark">
         <thead>
             <tr>
-                <th>Available Flights from {departsFrom} to {arrivesAt} with {NoOfStops} stop(s)</th>
+                <th>Available Flights with {NoOfStops} stop(s)</th>
             </tr>
         </thead>
         <tbody>
             {flights}
             <tr data-toggle="collapse" href="#{flightPathId}">
                 <td>
-                    {segmentIds}
-                    <i class="fa fa-caret-down float-right" aria-hidden="true"></i>
+                    <div>
+                        <i class="fa fa-plane" aria-hidden="true"></i>   {cityPath}<i class="fa fa-caret-down float-right" aria-hidden="true"></i>
+                    </div>
+                    <div>
+                        <i class="fa fa-clock-o" aria-hidden="true"></i>   {totalDepartureDate} <i class="fa fa-long-arrow-right" aria-hidden="true"></i> {totalArrivalTime}
+                    </div>
+                    <div></div>
                 </td>
             </tr>
             <tr id="{flightPathId}" class="collapse">
