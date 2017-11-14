db86d42882af8f2c7fb0c6814cd85be8249a96e1
Andy Lin <andylin1218@gmail.com>
2017-11-14 00:12:02 -0800

    Fixed the add plane link

--- application/views/addplane.php
+++ application/views/addplane.php
@@ -1,7 +1,7 @@
 <tbody>
     <tr>
         <td colspan="3" align="right">
-            <a href="/flights/add"><input type="button" class="btn btn-primary" value="Add new.."/></a>
+            <a href="/fleet/add"><input type="button" class="btn btn-primary" value="Add new.."/></a>
         </td>
     </tr>
 </tbody>
